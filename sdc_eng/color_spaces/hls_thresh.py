5765bd3dd8094f89a74223a0e87985890cd0c892
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-05-29 16:18:58 -0400

    added basic HLS thresholding

--- sdc_eng/color_spaces/hls_thresh.py
+++ sdc_eng/color_spaces/hls_thresh.py
@@ -8,10 +8,23 @@ image = mpimg.imread("test6.jpg")
 thresh = (180, 255)
 gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
-binary = np.zeros_like(gray)
-binary[(gray >= thresh[0]) & (gray <= thresh[1])] = 1
+#binary = np.zeros_like(gray)
+#binary[(gray >= thresh[0]) & (gray <= thresh[1])] = 1
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (24, 9))
+# R = image[:, :, 0]
+# binary = np.zeros_like(R)
+# binary[(R >= thresh[0]) & (R <= thresh[1])] = 1
+
+hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+S = hls[:, :, 2]
+
+thresh_s = (90, 150)
+
+binary = np.zeros_like(S)
+binary[(S > thresh[0]) & (S <= thresh[1])] = 1
+
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize = (16, 9))
 
 ax1.imshow(image)
 ax1.set_title("Original image")
@@ -19,6 +32,6 @@ ax1.set_title("Original image")
 ax2.imshow(binary, cmap = "gray")
 ax2.set_title("HLS thresholded")
 
-plt.subplots_adjust(left = 0., right = 1, top = 0.9, bottom = 0.)
+plt.subplots_adjust(left = 0., right = 1, top = 0.9, bottom = 0.1)
 
 plt.show()
