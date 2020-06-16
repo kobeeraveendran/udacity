81724faf4ba251288c61fa86a40369955e89f01a
Kobee Raveendran <kobee.raveendran@knights.ucf.edu>
2020-06-16 18:28:49 -0400

    cleaned up code

--- sdc_eng/advanced_cv/curvature.py
+++ sdc_eng/advanced_cv/curvature.py
@@ -11,9 +11,11 @@ def generate_data(ym_per_pix, xm_per_pix):
     # Set random seed number so results are consistent for grader
     # Comment this out if you'd like to see results on different random data!
     np.random.seed(0)
+    
     # Generate some fake data to represent lane-line pixels
     ploty = np.linspace(0, 719, num=720)# to cover same y-range as image
     quadratic_coeff = 3e-4 # arbitrary quadratic coefficient
+
     # For each y position generate random x position within +/-50 pix
     # of the line base position in each case (x=200 for left, and x=900 for right)
     leftx = np.array([200 + (y**2)*quadratic_coeff + np.random.randint(-50, high=51) 
@@ -54,13 +56,3 @@ if __name__ == "__main__":
     # pixel-space expected: 1625.06, 1976.30
     # real-world space expected: 533.75, 648.16
     print(left_curverad, right_curverad)
-
-    # plt.xlim(0, 1280)
-    # plt.ylim(0, 720)
-
-    #plt.plot(left_fitx, ploty, color = 'green', linewidth = 3)
-    #plt.plot(right_fitx, ploty, color = 'green', linewidth = 3)
-
-    #plt.gca().invert_yaxis()
-
-    #plt.show()
